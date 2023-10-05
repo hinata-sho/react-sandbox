5fb4269c82c62ba4ab24afd16480060ec5ce87d3
hinata-sho <114796071+hinata-sho@users.noreply.github.com>
2023-10-05 09:58:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CustomHookExample2 from './components/CustomHookExample2';
+
+function App() {
+  return (
+    <div className="conatiner mt-5">
+      <CustomHookExample2 />
+    </div>
+  );
+}
+
+export default App;
